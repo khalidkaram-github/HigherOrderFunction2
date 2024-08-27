98b0fd167e9c5f13d90d15d1a50d71897dde9c43
khalid karam <khalid.tasks@gmail.com>
2024-08-27 23:11:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace HigherOrderFunction2
+{
+    internal class Program
+    {
+
+        //Returning Functions from Functions
+        static int Add(int a, int b) => a + b;
+        static int Subtract(int a, int b) => a - b;
+
+        static void Main()
+        {
+            Func<int, int, int> add = GetMathOperation("add");
+            Console.WriteLine("Addition: " + add(5, 3)); // Output: 8
+            Console.WriteLine($"Addition: {GetMathOperation("add")(5, 3)}");// Output: 8
+
+
+
+
+            Func<int, int, int> subtract = GetMathOperation("subtract");
+            Console.WriteLine("Subtraction: " + subtract(5, 3)); // Output: 2
+            Console.WriteLine($"Subtraction: {GetMathOperation("subtract")(5, 3)}");// Output: 2
+        }
+        static Func<int, int, int> GetMathOperation(string operation)
+        {
+            switch (operation.ToLower())
+            {
+                case "add": return Add;
+                case "subtract": return Subtract;
+                default: throw new ArgumentException("Invalid operation");
+            }
+        }
+
+    }
+}
